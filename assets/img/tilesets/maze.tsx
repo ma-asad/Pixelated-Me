0525432ef4e5a38d62888f4f00d4c02ec4b73870
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2023-12-16 00:36:09 +0400

    gitignore

--- assets/img/tilesets/maze.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="maze" tilewidth="16" tileheight="16" tilecount="8375" columns="63">
- <image source="imgSource/maze.png" width="1008" height="528"/>
-</tileset>
--- assets/img/tilesets/tilesetFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilesetFloor" tilewidth="16" tileheight="16" tilecount="575" columns="25">
- <image source="imgSource/tilesetFloor.png" width="400" height="368"/>
-</tileset>
